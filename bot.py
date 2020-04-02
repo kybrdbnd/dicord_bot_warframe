981906af09308a31330dfb9ae9c23f6608807fc0
Pranav Puri <pranav@beamax.io>
2020-04-02 11:01:50 +0530

    roast command added

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 from utilities.all_utilities import *
-from utilities.constants import *
 
 
 @bot.event
@@ -30,35 +29,6 @@ async def on_member_join(member):
     if member.bot is False:
         await generalChannel.send(f"Hunter {member.mention}, Welcome to Warframe India Community!!!!. "
                                   f"Make sure to go to <#{introductionChannel.id}> channel to introduce yourself")
-
-
-@bot.command(name='say')
-async def say(ctx, *args):
-    if len(args) > 0:
-        await ctx.send(' '.join(args))
-    else:
-        await ctx.send("What do you wanna say!!!!")
-
-
-@bot.command(name='quotes')
-async def quotes(ctx):
-    simaris_quotes = cephalon_simaris_quotes
-    message = random.choice(simaris_quotes)
-    await ctx.send(message)
-
-
-@bot.command(name='capture')
-async def capture(ctx, members: commands.Greedy[discord.Member], *, sample='capture'):
-    author = ctx.author
-    message = ''
-    if len(members) > 0:
-        random_numbers = random.randint(1, len(members))
-        success_members = random.sample(members, random_numbers)
-        for member in success_members:
-            message += f"{member.mention}"
-        await ctx.send(f"{author.mention} you have synthesized {message}")
-    else:
-        await ctx.send("Hunter select targets to synthesize")
 
 
 bot.run(TOKEN)
